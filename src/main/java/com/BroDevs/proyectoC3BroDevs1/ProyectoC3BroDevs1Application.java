dc3eadcc729e14b4c0ea9bad5d8c4b9b156103f2
GTavx <tavalmuro@gmail.com>
2022-08-23 09:12:21 -0500

    Montaje de proyecto SpringBoot para q lo clonen

---
+++ src/main/java/com/BroDevs/proyectoC3BroDevs1/ProyectoC3BroDevs1Application.java
@@ -0,0 +1,13 @@
+package com.BroDevs.proyectoC3BroDevs1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoC3BroDevs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoC3BroDevs1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/BroDevs/proyectoC3BroDevs1/ProyectoC3BroDevs1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BroDevs.proyectoC3BroDevs1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoC3BroDevs1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
